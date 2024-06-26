2e4840f91d3e31b5931e9483b10e99a273c8b438
lsc58461 <lsc58461@naver.com>
2024-06-26 15:44:40 +0900

    fix: SpeedInsights 위치 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
-      <SpeedInsights />
+      <body className={inter.className}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
